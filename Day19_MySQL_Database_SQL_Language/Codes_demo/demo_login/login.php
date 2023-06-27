c766452f125b95688877efea29f81c7160deb11f
Nguyen Viet Manh <nguyenvietmanhit@gmail.com>
2023-06-27 09:03:40 +0700

    update

---
+++ Day19_MySQL_Database_SQL_Language/Codes_demo/demo_login/login.php
@@ -0,0 +1,15 @@
+<!--login.php-->
+<form action="" method="post">
+	Username:
+	<input type="text" name="username">
+	<br>
+	Password:
+	<input type="password" name="password">
+	<br>
+<!-- Nếu chỉ có duy nhất 1 checkbox thì name ko cần
+	ở dạng mảng-->
+	<input type="checkbox" name="remember">
+	Ghi nhớ đăng nhập
+	<br>
+	<input type="submit" name="login" value="Đăng nhập">
+</form>
