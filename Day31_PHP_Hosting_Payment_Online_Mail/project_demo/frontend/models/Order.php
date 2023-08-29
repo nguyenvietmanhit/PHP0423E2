ff5f18fb8c0718f257f7dad3189a2ed19ebeb5d7
Nguyen Viet Manh <nguyenvietmanhit@gmail.com>
2023-08-29 13:22:41 +0700

    a

--- Day31_PHP_Hosting_Payment_Online_Mail/project_demo/frontend/models/Order.php
+++ Day31_PHP_Hosting_Payment_Online_Mail/project_demo/frontend/models/Order.php
@@ -1,5 +1,26 @@
 <?php
 require_once 'models/Model.php';
 class Order extends Model {
-
+	
+	public function insertOrder($fullname, $address, $mobile, $email, $note, $price_total, $payment_status) {
+		$sql_insert = "INSERT INTO orders(fullname, address, mobile, email, note, price_total, payment_status)
+    VALUES (:fullname, :address, :mobile, :email, :note, :price_total, :payment_status)";
+		$obj_insert = $connection->prepare($sql_insert);
+		$arr_insert = [
+			':fullname' => $fullname,
+			':address' => $address,
+			':mobile' => $mobile,
+			':email' => $email,
+			':note' => $note,
+			':price_total' => $price_total,
+			':payment_status' => $payment_status,
+		];
+		$obj_insert->execute($arr_insert);
+		// Trả về id của order vừa mới insert thành công
+		$order_id = $this->connection->lastInsertId();
+		
+		return $order_id;
+//    return $obj_insert->execute($arr_insert);
+	}
+	
 }
